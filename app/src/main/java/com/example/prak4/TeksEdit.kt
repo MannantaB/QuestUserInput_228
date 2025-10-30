a2831c584cec6b78d77ae1c5f6ecc9e9442ad6c6
MannantaB <mannanta18@gmail.com>
2025-10-30 08:53:31 +0700

    add mutable state untuk form input (nama, alamat, jk)

--- app/src/main/java/com/example/prak4/TeksEdit.kt
+++ app/src/main/java/com/example/prak4/TeksEdit.kt
@@ -1,10 +1,16 @@
 package com.example.prak4
 
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier
 ){
-
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
 }
