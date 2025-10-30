40abb89b1936dd517a25fa8451aeddc408f752c4
MannantaB <mannanta18@gmail.com>
2025-10-30 08:54:57 +0700

    add variabek untuk menyimpan data dari input

--- app/src/main/java/com/example/prak4/TeksEdit.kt
+++ app/src/main/java/com/example/prak4/TeksEdit.kt
@@ -13,4 +13,8 @@ fun FormDataDiri(modifier: Modifier
     var textNama by remember { mutableStateOf(value = "") }
     var textAlamat by remember { mutableStateOf(value = "") }
     var textJK by remember { mutableStateOf(value = "") }
+
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
 }
