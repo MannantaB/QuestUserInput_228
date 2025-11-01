c7272882f8306585fe7ecc8a78d3ae9d1d5117f2
MannantaB <mannanta18@gmail.com>
2025-11-01 08:06:21 +0700

    add variabel untuk menyimpan inputan status

--- app/src/main/java/com/example/prak4/TeksEdit.kt
+++ app/src/main/java/com/example/prak4/TeksEdit.kt
@@ -39,6 +39,7 @@ fun FormDataDiri(modifier: Modifier
     var nama by remember { mutableStateOf(value = "") }
     var alamat by remember { mutableStateOf(value = "") }
     var jenis by remember { mutableStateOf(value = "") }
+    var status by remember { mutableStateOf("") }
 
     val gender: List<String> = listOf("Laki-laki", "Perempuan")
 
