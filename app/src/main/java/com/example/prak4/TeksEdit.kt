fc3dbe66a69e8e8e656c87260fb21f7383e5e91d
MannantaB <mannanta18@gmail.com>
2025-11-01 08:13:06 +0700

    add spacer

--- app/src/main/java/com/example/prak4/TeksEdit.kt
+++ app/src/main/java/com/example/prak4/TeksEdit.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -72,6 +73,8 @@ fun FormDataDiri(modifier: Modifier
                     modifier = Modifier.fillMaxWidth()
                 )
             }
+
+            Spacer(modifier = Modifier.height(20.dp))
         }
 
         Row {
